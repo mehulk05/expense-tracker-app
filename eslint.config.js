f6ef6f1e42d82d9569dbca34f1e959fc16cce987
mehul kothari <mehul.kothari@growth99.com>
2025-05-03 11:58:51 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,70 @@
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'readonly', // Make React available globally
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // formatting
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'no-mixed-spaces-and-tabs': 'error',
+
+      // import sorting - enhanced rules
+      'import/first': 'error', // Ensures all imports are at the top of the file
+      'import/no-duplicates': 'error', // Prevents duplicate imports
+      'import/newline-after-import': 'error', // Enforces a newline after import statements
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              "pattern": "assets/**",
+              "group": "internal",
+              "position": "before"
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  }
+);
